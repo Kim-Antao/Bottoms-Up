8dc5f92928ff18fcdb4a1ea37966e96cca324373
Kimberley Antao <kimberley2609@gmail.com>
2024-09-09 23:49:26 +0100

    added search functionality

---
+++ src/components/SearchBox/SearchBox.tsx
@@ -0,0 +1,18 @@
+import "./SearchBox.scss";
+import { FormEvent } from "react";
+
+type searchBoxProps = {
+    searchTerm: string,
+    handleSearchInput: (event: FormEvent<HTMLInputElement>) => void,
+}
+
+const SearchBox = ({searchTerm, handleSearchInput}:searchBoxProps) => {
+  return (
+    <div>
+        <input className="search-box" type="text" value={searchTerm} onInput={handleSearchInput} placeholder="Drink name"/>
+      
+    </div>
+  )
+}
+
+export default SearchBox
