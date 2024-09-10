d2c616cb94e100286434c5437fb5b077cd55f29e
Kimberley Antao <kimberley2609@gmail.com>
2024-09-10 10:09:24 +0100

    moved navigation out of this component to make use of routing better

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,29 +1,25 @@
-//import React from 'react'
 import './LandingPage.scss';
 import Nav from '../Nav/Nav';
 
 type LandingPageProps = {
-    video:string,
-    heading:string,
+    video: string,
+    heading: string,
+    para: string,
     buttonLabel: string
 }
 
-const LandingPage = ({video, heading, buttonLabel}: LandingPageProps) => {
+const LandingPage = ({video, heading, para, buttonLabel}: LandingPageProps) => {
   return (
 
     <div className="main-display">
-      <Nav/>
-
-      {/* <img className="main-display__image" src={image} alt="" /> */}
-      <video className="main-display__video" src={video} autoPlay loop></video>
+      <video className="main-display__video" autoPlay loop>
+        <source src={video} type='video/mp4'/>
+      </video>
       <div className="main-display__content">
-      <h1>{heading}</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-      </p>
-      <button>{buttonLabel}</button>
+        <h1 className="main-display__content--heading-font">{heading}</h1>
+        <p className="main-display__para">{para}</p>
+        <button className="main-display__button">{buttonLabel}</button>
       </div>
-      
     </div>
   )
 }
