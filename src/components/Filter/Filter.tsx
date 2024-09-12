96dc7f807d9190d7ca8876630f12699e68fd409d
Kimberley Antao <kimberley2609@gmail.com>
2024-09-12 17:53:15 +0100

    filter component styling and refactoring completed

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,26 +1,24 @@
-//import React from 'react'
 import './Filter.scss';
 import { FormEventHandler } from "react"
 
-type FilterProps ={
+type FilterProps = {
   drinkCategory: string,
-  handleTypeSelected: (FormEventHandler<HTMLInputElement>)
+  handleTypeSelected: (FormEventHandler<HTMLInputElement>),
 }
 
 const Filter = ({drinkCategory,handleTypeSelected}:FilterProps) => {
  
-
   return (
-   <div className='filter-container'>
-    <label className='filter-container__item-label' htmlFor="allDrinks">All </label>
+    <div className='filter-container'>
+      <label className='filter-container__item-label' htmlFor="allDrinks">All </label>
       <input className='filter-container__item-radiobtn' type="radio" name="drink-category" value="all" id="allDrinks" checked={drinkCategory==="all"} onChange={handleTypeSelected} />
-     
-    <label className='filter-container__item-label' htmlFor="alcDrinks">Alcoholic </label>
+
+      <label className='filter-container__item-label' htmlFor="alcDrinks">Alcoholic </label>
       <input className='filter-container__item-radiobtn' type="radio" name="drink-category" value="alcoholic" id="alcDrinks" onChange={handleTypeSelected} />
-   
-    <label className='filter-container__item-label' htmlFor="nonAlcDrinks">Non-Alcoholic</label>
+
+      <label className='filter-container__item-label' htmlFor="nonAlcDrinks">Non-Alcoholic</label>
       <input className='filter-container__item-radiobtn' type="radio" name="drink-category" value="non-alcoholic" id="nonAlcDrinks" onChange={handleTypeSelected} />
-  </div>
+    </div>
   )
 }
 
