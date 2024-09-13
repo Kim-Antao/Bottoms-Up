d105e91794cf173b8d661d32348775a6dded9880
Kimberley Antao <kimberley2609@gmail.com>
2024-09-13 15:24:00 +0100

    issue with random drink component resolved

--- src/container/RandomDrinkDisplay/RandomDrinkDisplay.tsx
+++ src/container/RandomDrinkDisplay/RandomDrinkDisplay.tsx
@@ -5,18 +5,25 @@ import Recipe from "../../components/Recipe/Recipe";
 
 const RandomDrinkDisplay = () => {
   const [randomDrink, setRandomDrink] = useState({} as Drink);
+  const [isLoading, setIsLoading] = useState(true);
   const url = 'https://thecocktaildb.com/api/json/v1/1/random.php';
   const ingredientsWithMeasurements = getIngredientsWithMeasurements(randomDrink);
 
   useEffect(()=>{
       fetch(url)
       .then(response=>response.json())
-      .then(data=>setRandomDrink(data.drinks[0]));
+      .then(data=>{
+        setRandomDrink(data.drinks[0])
+        setIsLoading(false);
+      });
   },[])
 
+/*   useEffect(()=>{
+    setIsLoading(!isLoading);
+  },[randomDrink]) */
   return (
     <>
-      <Recipe recipe={randomDrink} ingredientsWithMeasurements={ingredientsWithMeasurements} key={`${randomDrink.idDrink}2`}/>
+      {!isLoading && randomDrink && <Recipe recipe={randomDrink} ingredientsWithMeasurements={ingredientsWithMeasurements} key={`${randomDrink.idDrink}2`}/>}
     </>
   )
 }
